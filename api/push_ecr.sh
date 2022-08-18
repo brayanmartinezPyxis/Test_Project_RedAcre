71798d4dc714cc48b173ab886901d7f54ee7cbbc
Brayan Martinez <brayan.martinez@usantotomas.edu.co>
2022-08-18 14:41:32 +0200

    challenge

--- api/push_ecr.sh
+++ api/push_ecr.sh
@@ -1,3 +1,5 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 753941739980.dkr.ecr.us-east-1.amazonaws.com
-docker tag backend:latest 753941739980.dkr.ecr.us-east-1.amazonaws.com/backend:latest
-docker push 753941739980.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+accountId=""
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $accountId.dkr.ecr.us-east-1.amazonaws.com
+docker tag backend:latest $accountId.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+docker push $accountId.dkr.ecr.us-east-1.amazonaws.com/backend:latest
